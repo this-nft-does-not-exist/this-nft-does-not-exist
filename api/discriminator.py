0cfe44d4c710061d274a90a4ab42d2593f3a05e2
Harit Kapadia <haritkapadia@outlook.com>
2022-09-17 19:25:07 +0000

    New file for ML

--- api/discriminator.py
+++ api/discriminator.py
@@ -11,15 +11,21 @@ import time
 
 def make_discriminator_model():
     model = tf.keras.Sequential()
-    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                            input_shape=[28, 28, 3]))
+    model.add(layers.Conv2D(
+        64,
+        (5, 5),
+        strides=(2, 2),
+        padding="same",
+        input_shape=[28, 28, 3]
+    ))
+
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
 
     model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
-
     model.add(layers.Flatten())
     model.add(layers.Dense(1))
 
---
+++ api/get-data.py
@@ -0,0 +1,46 @@
+#!/bin/env python3
+import io
+import sqlite3
+import sys
+import numpy
+import PIL.Image
+import requests
+import tqdm
+
+CONNECTIONS = 100
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0",
+}
+URL = "https://nonfungible.com/api/salesForProject"
+PARAMS = {
+    "filter": '{"blockTimestamp":["2021-04-23T00:00:00.000-04:00","2022-09-17T23:59:59.999-04:00"]}',
+    "project": "boredapeclub",
+    "limit": 100,
+    "orderBy": "blockTimestamp",
+    "order": "DESC",
+}
+CREATE_QUERY = "CREATE TABLE nft (image TEXT NOT NULL, data BLOB NOT NULL)"
+INSERT_QUERY = "INSERT INTO nft (image, data) VALUES(?, ?)"
+
+
+def download(cursor, url):
+    res = requests.get(url, headers=HEADERS)
+    image = PIL.Image.open(io.BytesIO(res.content)).convert("RGB")
+    cursor.execute(INSERT_QUERY, (url, numpy.asarray(image).tobytes()))
+
+
+def main():
+    items = []
+
+    items = requests.get(URL, params=PARAMS, headers=HEADERS).json()["sales"]
+
+    with sqlite3.connect(sys.argv[1]) as con:
+        cur = con.cursor()
+        cur.execute(CREATE_QUERY)
+
+        for item in tqdm.tqdm(items):
+            download(cur, f'https://nonfungible.com/_next/image?url=http://images1.nonfungible.com:3333/api/v4/asset/media/image/{item["project"]}/{item["nftTicker"]}/{item["assetId"]}&w=32&q=100')
+
+
+if __name__ == "__main__":
+    main()
