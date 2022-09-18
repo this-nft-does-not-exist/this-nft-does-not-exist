23f702374202af1deaf8ab9d4898da544117d0bf
ananyavishwanath <83526224+ananyavishwanath@users.noreply.github.com>
2022-09-18 01:31:28 -0400

    Update server.py

--- api/server.py
+++ api/server.py
@@ -7,13 +7,18 @@ app = flask.Flask(__name__)
 run.restore_checkpoint()
 
 
-@app.get("/")
+@app.get("/api/image")
 def image():
     im = run.generate_image()
     img_io = io.BytesIO()
     im.save(img_io, 'PNG')
     img_io.seek(0)
-    return flask.send_file(img_io, mimetype='image/png')
+    res = flask.send_file(img_io, mimetype='image/png')
+    res.headers.add("Access-Control-Allow-Origin", "*")
+    return res
 
+@app.get("/")
+def index():
+    return flask.send_file("index.html")
 
-app.run(port=int(sys.argv[1]) if len(sys.argv) > 0 else 1234)
+app.run(port=1234 if len(sys.argv) <=1 else int(sys.argv[1]))
