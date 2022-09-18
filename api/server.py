c58725a259bf3392b6b2f98c5e21404c4dad2fef
Harit Kapadia <haritkapadia@outlook.com>
2022-09-18 01:53:25 +0000

    Add server

--- api/server.py
+++ api/server.py
@@ -1,10 +1,23 @@
+import io
 import flask
 import run
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_url_path="", static_folder="../web/build/web")
+run.restore_checkpoint()
+
+
+@app.get("/api/image")
+def image():
+    im = run.generate_image()
+    img_io = io.BytesIO()
+    im.save(img_io, 'PNG')
+    img_io.seek(0)
+    return flask.send_file(img_io, mimetype='image/png')
+
 
 @app.get("/")
 def index():
-    return "Yo!"
+    return flask.redirect("/index.html")
+
 
 app.run(port=3000)
